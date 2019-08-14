e09684b391d3ca01d3626abb92b2f78b39e7af29
Philipp_Tarasiewicz <Philipp_Tarasiewicz@symantec.com>
2019-08-14 13:21:34 +0200

    add example component

--- src/app/top-level-a/top-level-a.module.ts
+++ src/app/top-level-a/top-level-a.module.ts
@@ -6,9 +6,10 @@ import { TopLevelAHomeComponent } from './top-level-a-home/top-level-a-home.comp
 import { TopLevelAOneComponent } from './top-level-a-one/top-level-a-one.component';
 import { TopLevelATwoComponent } from './top-level-a-two/top-level-a-two.component';
 import { TopLevelAThreeComponent } from './top-level-a-three/top-level-a-three.component';
+import { ExampleComponent } from './top-level-a-one/example/example.component';
 
 @NgModule({
-  declarations: [TopLevelAHomeComponent, TopLevelAOneComponent, TopLevelATwoComponent, TopLevelAThreeComponent],
+  declarations: [TopLevelAHomeComponent, TopLevelAOneComponent, TopLevelATwoComponent, TopLevelAThreeComponent, ExampleComponent],
   imports: [
     CommonModule,
     TopLevelARoutingModule
