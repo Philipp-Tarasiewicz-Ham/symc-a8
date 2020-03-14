5e6102178b0b55d437bbc3f661708d650bf6f90d
Philipp_Tarasiewicz <Philipp_Tarasiewicz@symantec.com>
2020-03-14 16:45:22 +0100

    establish better action naming

--- src/app/top-level-c/store/top-level-c-one.actions.ts
+++ src/app/top-level-c/store/top-level-c-one.actions.ts
@@ -1,5 +1,5 @@
 import { CONFIG } from './top-level-c-one.config';
 import {createAction, props} from '@ngrx/store';
 
-export const loadTodos = createAction(`[${CONFIG.NAME}] Load Todos`);
-export const loadTodosSuccess = createAction(`[${CONFIG.NAME}] Load Todos Success`, props<{ payload }>());
+export const loadTodos = createAction(`[${CONFIG.NAME}] Load TODOs`);
+export const loadTodosSuccess = createAction(`[${CONFIG.NAME}] TODOs Successfully Loaded`, props<{ payload }>());
